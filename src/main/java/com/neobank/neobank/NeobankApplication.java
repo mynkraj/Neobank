8fcec8896ab9ab561dd20273319e32bb3b2c6d78
Mayank Rajput <mayankrajput@Mayanks-MacBook-Air.local>
2026-04-03 23:37:48 +0530

    Initial Project Setup

---
+++ src/main/java/com/neobank/neobank/NeobankApplication.java
@@ -0,0 +1,13 @@
+package com.neobank.neobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeobankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeobankApplication.class, args);
+    }
+
+}
